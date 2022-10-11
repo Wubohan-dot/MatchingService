2f344672e23e2c6392aa70358982d8faab1db651
postgre <819186192@qq.com>
2022-10-11 21:58:38 +0800

    generate content of CSV wholly in memory instead of file

--- router.go
+++ router.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/csv"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -45,23 +43,40 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	filename, err := makeTmpFile(resp)
+	// filename, err := makeTmpFile(resp)
+
+	filename := generateFileName()
 
 	// https://segmentfault.com/a/1190000020202158
-	file, err := os.Open(filename)
-	content, err := ioutil.ReadAll(file)
-	file.Close()
+	// file, err := os.Open(filename)
+	content := csvFormatResp(resp)
+	log.Printf("%+v", content)
+	// file.Close()
 	fileName := url.QueryEscape(filename) // to avoid Chinese
 	w.Header().Add("Content-Type", "application/octet-stream")
 	w.Header().Add("Content-Disposition", "attachment; filename=\""+fileName+"\"")
 	w.Write(content)
 
-	defer func() {
-		err = os.Remove(filename)
-		if err != nil {
-			fmt.Println("remove  excel file failed", err)
+	//defer func() {
+	//	err = os.Remove(filename)
+	//	if err != nil {
+	//		fmt.Println("remove  excel file failed", err)
+	//	}
+	//}()
+}
+
+func csvFormatResp(resp [][]string) []byte {
+	var s string
+	// [239 187 191 65 44 66 44 67 10 97 49 44 98 49 44 99 49 10]
+	for _, row := range resp {
+		for _, data := range row[:len(row)-1] {
+			s += data
+			s += ","
 		}
-	}()
+		s += row[len(row)-1]
+		s += string(rune(10))
+	}
+	return []byte(s)
 }
 
 func generateFileName() string {
